0867480e53a7576efe85b691a74411ed2d4dda46
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-26 01:59:56 +0530

    Add files via upload

---
+++ basic_problems/count_digit.java
@@ -0,0 +1,22 @@
+package basic_problems;
+import java.util.Scanner;
+
+public class count_digit{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while(num != 0){
+            num = num / 10 ;
+            count++;
+        }
+        System.out.println("Number of digit is : "+count);
+
+        sc.close();
+}
+}
