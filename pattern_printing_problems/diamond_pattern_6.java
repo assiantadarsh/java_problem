0ee081a84a30e00ff9d3d41088aa44bdbf22a2c2
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-27 00:33:46 +0530

    Add files via upload

---
+++ pattern_printing_problems/diamond_pattern_6.java
@@ -0,0 +1,61 @@
+package pattern_problem;
+import java.util.Scanner;
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+public class diamond_pattern_6 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the positive value of n : ");
+
+        int n = sc.nextInt();
+
+        if(n <= 0){
+            System.out.println("Not possible please enter positive value ..");
+        }
+
+        // Upper pyramid 
+
+        int nsp = 1;
+        for(int i = 1; i<= n;i++){
+
+            for(int k = 1;k <= n-i;k++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= nsp;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            nsp += 2;
+        }
+
+        // Lower Pyramid
+
+        int lsp = (2 *n) - 3;
+
+        for(int i = 1; i<= n - 1;i++){
+
+            for(int k = 1;k <= i;k++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= lsp ;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            lsp -= 2;
+        }
+
+
+        sc.close();
+    }
+}
---
+++ pattern_printing_problems/hollow_square_7.java
@@ -0,0 +1,39 @@
+package pattern_problem;
+import java.util.Scanner;
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+public class hollow_square_7 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the positive value of n : ");
+
+        int n = sc.nextInt();
+
+        if(n <= 0){
+            System.out.println("Not possible please enter positive value ..");
+        }
+
+        for(int i = 1; i<= n;i++){
+            for(int j = 1; j <= n;j++){
+                if(i == 1 || i == n){
+                    System.out.print("*");
+                }
+                else if(j == 1 || j == n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+        sc.close();
+    }
+}
