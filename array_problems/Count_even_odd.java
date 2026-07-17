20d492824428c732920db158ff57052352a4ebd7
Adarsh shukla <adarshbhai917963@gmail.com>
2026-07-17 22:20:47 +0530

    Add Count_even_odd.java to count even and odd numbers

---
+++ array_problems/Count_even_odd.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+
+        int even = 0;
+        int odd = 0;
+
+        for(int num : arr){
+            if(num % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Even = " + even);
+        System.out.println("Odd = " + odd);
+    }
+}
