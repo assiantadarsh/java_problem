fd883633235ef69f37eb06b12272935c76e743c8
Adarsh shukla <adarshbhai917963@gmail.com>
2026-07-18 21:51:07 +0530

    Add second largest number finder in second_largest.java
    Implement logic to find the second largest number in an array.

---
+++ array_problems/second_largest.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {12,35,1,10,34,1};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int num : arr){
+            if(num > largest){
+                second = largest;
+                largest = num;
+            }else if(num > second && num != largest){
+                second = num;
+            }
+        }
+
+        System.out.println(second);
+    }
+                     }
