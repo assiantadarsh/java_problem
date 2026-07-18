70c15f02adaaca5b55537cd4a8179b1d6a3be576
Adarsh shukla <adarshbhai917963@gmail.com>
2026-07-18 21:52:23 +0530

    Add array sorted check in Main class
    Implement check for sorted array in Main class.

---
+++ array_problems/check_array_sorted.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40};
+
+        boolean sorted = true;
+
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i] > arr[i+1]){
+                sorted = false;
+                break;
+            }
+        }
+
+        System.out.println(sorted);
+    }
+}
