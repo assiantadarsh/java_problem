4ada9ca5a4faeef38b2460656796402b6504371f
Adarsh shukla <adarshbhai917963@gmail.com>
2026-07-19 21:54:58 +0530

    Create move_all_zeros.java

---
+++ array_problems/move_all_zeros.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,0,3,4,0};
+
+        int index = 0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] != 0){
+                arr[index++] = arr[i];
+            }
+        }
+
+        while(index < arr.length){
+            arr[index++] = 0;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
