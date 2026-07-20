36f317e19775b7f21ddd9467484b17ec8ecfdd7a
Adarsh shukla <adarshbhai917963@gmail.com>
2026-07-20 21:35:45 +0530

    Create rotate_by_k.java

---
+++ array_problems/rotate_by_k.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int k = 2;
+
+        k = k % arr.length;
+
+        for(int r = 0; r < k; r++) {
+            int last = arr[arr.length - 1];
+
+            for(int i = arr.length - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[0] = last;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+          }
