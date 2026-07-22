1e553b057423726c0f6a4f1bb70e5a5e7aa5a5bd
Adarsh shukla <adarshbhai917963@gmail.com>
2026-07-22 22:18:50 +0530

    Create remove_duplicates.java

---
+++ array_problems/remove_duplicates.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int j = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        System.out.println("Array after removing duplicates:");
+
+        for (int i = 0; i <= j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
